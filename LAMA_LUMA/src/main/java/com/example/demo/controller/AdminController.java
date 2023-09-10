dc30c1e08d8a4e03e5014b8906718f0c568e5175
dhaniyapatta <ajha211@gmail.com>
2023-09-11 00:01:33 +0530

    Added login message

--- LAMA_LUMA/src/main/java/com/example/demo/controller/AdminController.java
+++ LAMA_LUMA/src/main/java/com/example/demo/controller/AdminController.java
@@ -17,11 +17,20 @@ public class AdminController {
 	
 	@GetMapping("/")
 	public String sayHEllo() {
-		return "Heelo deer";
+		return "Hello deer";
 	}
 	@PostMapping("/addAdmin")
 	public Admin addAdmin(@RequestBody Admin admin) {
 		return (Admin) adminRepo.save(admin) ;
+	}
+
+	@PostMapping("/login")
+	public String validateLogin(@RequestBody Admin admin) {
+		Admin tempAdmin=adminRepo.getReferenceById(admin.getAdminId());
+		if(tempAdmin.getPassword().equals(admin.getPassword()))
+			return "Successful Login";
+		else
+			return "Failed to Log In! Please Try Again";
 	}
 
 }
