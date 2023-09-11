b309180c06c630bded8e84d630b2326e805c94a8
shreya <shreya.srikrishna@gmail.com>
2023-09-11 20:13:09 +0530

    added admin login authentication

--- LAMA_LUMA/src/main/java/com/example/demo/controller/AdminController.java
+++ LAMA_LUMA/src/main/java/com/example/demo/controller/AdminController.java
@@ -9,20 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Admin;
 import com.example.demo.repository.AdminRepository;
+import com.example.demo.service.AdminService;
 
 @RestController
 public class AdminController {
 	@Autowired
 	private AdminRepository adminRepo;
+	@Autowired
+	private AdminService adminService;
 	
-	@GetMapping("/")
-	public String sayHEllo() {
-		return "Heelo deer";
-	}
+	
 	@PostMapping("/addAdmin")
 	public Admin addAdmin(@RequestBody Admin admin) {
 		return (Admin) adminRepo.save(admin) ;
 	}
 	//for push
+	
+	@PostMapping("/adminLogin")
+	public String validateAdminLogin(@RequestBody Admin admin) {
+		Admin tempAdmin = adminRepo.getReferenceById(admin.getAdminId());
+		return tempAdmin.getPassword().equals(admin.getPassword()) ? "pass" : "fail";
+	}
 
 }
