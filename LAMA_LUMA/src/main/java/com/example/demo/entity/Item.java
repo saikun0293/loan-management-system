9957b35e550b9ec240e7aa6b88fd962a5be8b064
dhaniyapatta <ajha211@gmail.com>
2023-09-14 12:39:21 +0530

    added entities

---
+++ LAMA_LUMA/src/main/java/com/example/demo/entity/Item.java
@@ -0,0 +1,85 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Id;
+
+public class Item {
+	
+	@Id
+	public String itemId;
+	public String itemDesc;
+	public char itemStatus;
+	public String itemMake;
+	public String itemCategory;
+	public int itemValue;
+
+	
+	public Item() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getItemId() {
+		return itemId;
+	}
+
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+
+	public String getItemDesc() {
+		return itemDesc;
+	}
+
+
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+
+
+	public char getItemStatus() {
+		return itemStatus;
+	}
+
+
+	public void setItemStatus(char itemStatus) {
+		this.itemStatus = itemStatus;
+	}
+
+
+	public String getItemMake() {
+		return itemMake;
+	}
+
+
+	public void setItemMake(String itemMake) {
+		this.itemMake = itemMake;
+	}
+
+
+	public String getItemCategory() {
+		return itemCategory;
+	}
+
+
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
+	}
+
+
+	public int getItemValue() {
+		return itemValue;
+	}
+
+
+	public void setItemValue(int itemValue) {
+		this.itemValue = itemValue;
+	}
+	
+	
+	
+	
+
+}
